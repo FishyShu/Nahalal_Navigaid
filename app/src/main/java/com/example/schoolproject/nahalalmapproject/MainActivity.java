a1ef6926b64a837c526a56f406e5ff1e7249425a
FishyShu Lap <tomer.efrat.2005@gmail.com>
2023-02-20 16:49:57 +0200

    It just Works!

--- app/src/main/java/com/example/schoolproject/nahalalmapproject/MainActivity.java
+++ app/src/main/java/com/example/schoolproject/nahalalmapproject/MainActivity.java
@@ -1,21 +1,57 @@
 package com.example.schoolproject.nahalalmapproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottom_navigation;
     TextView ph_MA;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottom_navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottom_navigation.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent = null;
+
+                switch (item.getItemId()) {
+                    case R.id.navigation_map:
+                        intent = new Intent(MainActivity.this, MapActivity.class);
+                        break;
+                    case R.id.navigation_favorite:
+                        intent = new Intent(MainActivity.this, FavoriteActivity.class);
+                        break;
+                    case R.id.navigation_list:
+                        intent = new Intent(MainActivity.this, ListActivity.class);
+                        break;
+                    case R.id.navigation_settings:
+                        intent = new Intent(MainActivity.this, SettingsActivity.class);
+                        break;
+                    default:
+                        break;
+                }
+
+                if (intent != null) {
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
         //THe following is a placeholder
         ph_MA = findViewById(R.id.ph_MA);
         ph_MA.setOnClickListener(new View.OnClickListener() {
@@ -24,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, SignInSignUp.class);
                 startActivity(i);
             }
-        });
+        }); //testing / continuing the sign in / sign up
 
     }
 }
